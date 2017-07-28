eca20acb46e6048f314937f649f53eb81fdefaf1
Markus Morawitz <markus.morawitz@gmx.com>
2017-07-28 07:59:41 +0200

    Setup initial project using AnotherJiraRestClient
    see https://github.com/peoplesbeer/AnotherJiraRestClient commit 385ba09
    from 24. Jun 2016

---
+++ JiraProjects/JiraProjectsMainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JiraProjects
+{
+    public partial class JiraProjectsMainForm : Form
+    {
+        public JiraProjectsMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
