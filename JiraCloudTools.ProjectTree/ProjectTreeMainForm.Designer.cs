3ed19c4dc6e2cf293aba594b6a9d82205a2335f1
Markus Morawitz <markus.morawitz@gmx.com>
2017-08-01 12:03:48 +0200

    Using advanced tree view

--- JiraCloudTools.ProjectTree/ProjectTreeMainForm.Designer.cs
+++ JiraCloudTools.ProjectTree/ProjectTreeMainForm.Designer.cs
@@ -28,28 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup1", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("ListViewGroup2", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "item1",
-            "eins",
-            "zwei",
-            "drei"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new System.Windows.Forms.ListViewItem.ListViewSubItem[] {
-            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "item2"),
-            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "subitem1"),
-            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "subitem2"),
-            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "subitem3", System.Drawing.SystemColors.MenuHighlight, System.Drawing.SystemColors.Window, new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)))),
-            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "subitem4")}, -1);
             this.jiraConnectBarControl = new JiraCloudTools.Components.JiraConnectBarControl();
-            this.treeView = new System.Windows.Forms.TreeView();
-            this.listView = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeViewAdv = new Aga.Controls.Tree.TreeViewAdv();
+            this.nodeTextBox = new Aga.Controls.Tree.NodeControls.NodeTextBox();
             this.SuspendLayout();
             // 
             // jiraConnectBarControl
@@ -58,85 +39,42 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.jiraConnectBarControl.Location = new System.Drawing.Point(12, 12);
             this.jiraConnectBarControl.Name = "jiraConnectBarControl";
-            this.jiraConnectBarControl.Size = new System.Drawing.Size(957, 25);
+            this.jiraConnectBarControl.Size = new System.Drawing.Size(766, 25);
             this.jiraConnectBarControl.TabIndex = 0;
             this.jiraConnectBarControl.ConnectedToJiraClient += new System.EventHandler<JiraCloudTools.Components.JiraConnectBarControl.ConnectedEventArgs>(this.jiraConnectBarControl_ConnectedToJiraClient);
             // 
-            // treeView
+            // treeViewAdv
             // 
-            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeView.Location = new System.Drawing.Point(12, 43);
-            this.treeView.Name = "treeView";
-            this.treeView.Size = new System.Drawing.Size(957, 191);
-            this.treeView.TabIndex = 1;
-            // 
-            // listView
-            // 
-            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.treeViewAdv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4});
-            this.listView.GridLines = true;
-            listViewGroup1.Header = "ListViewGroup1";
-            listViewGroup1.Name = "listViewGroup1";
-            listViewGroup2.Header = "ListViewGroup2";
-            listViewGroup2.Name = "listViewGroup2";
-            this.listView.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-            listViewGroup1,
-            listViewGroup2});
-            listViewItem1.Checked = true;
-            listViewItem1.Group = listViewGroup1;
-            listViewItem1.StateImageIndex = 1;
-            listViewItem2.Group = listViewGroup2;
-            listViewItem2.IndentCount = 3;
-            this.listView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2});
-            this.listView.Location = new System.Drawing.Point(12, 240);
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(957, 161);
-            this.listView.TabIndex = 2;
-            this.listView.UseCompatibleStateImageBehavior = false;
-            this.listView.View = System.Windows.Forms.View.Details;
-            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            this.treeViewAdv.BackColor = System.Drawing.SystemColors.Window;
+            this.treeViewAdv.DefaultToolTipProvider = null;
+            this.treeViewAdv.DragDropMarkColor = System.Drawing.Color.Black;
+            this.treeViewAdv.LineColor = System.Drawing.SystemColors.ControlDark;
+            this.treeViewAdv.Location = new System.Drawing.Point(12, 43);
+            this.treeViewAdv.Model = null;
+            this.treeViewAdv.Name = "treeViewAdv";
+            this.treeViewAdv.NodeControls.Add(this.nodeTextBox);
+            this.treeViewAdv.SelectedNode = null;
+            this.treeViewAdv.Size = new System.Drawing.Size(766, 358);
+            this.treeViewAdv.TabIndex = 1;
+            this.treeViewAdv.Text = "treeViewAdv1";
             // 
-            // columnHeader1
+            // nodeTextBox
             // 
-            this.columnHeader1.Text = "ColumnHeader1";
-            this.columnHeader1.Width = 191;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "ColumnHeader2";
-            this.columnHeader2.Width = 189;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "ColumnHeader3";
-            this.columnHeader3.Width = 155;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "ColumnHeader4";
-            // 
-            // imageList1
-            // 
-            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.nodeTextBox.DataPropertyName = "Text";
+            this.nodeTextBox.EditEnabled = true;
+            this.nodeTextBox.IncrementalSearchEnabled = true;
+            this.nodeTextBox.LeftMargin = 3;
+            this.nodeTextBox.ParentColumn = null;
             // 
             // ProjectTreeMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(981, 413);
-            this.Controls.Add(this.listView);
-            this.Controls.Add(this.treeView);
+            this.ClientSize = new System.Drawing.Size(790, 413);
+            this.Controls.Add(this.treeViewAdv);
             this.Controls.Add(this.jiraConnectBarControl);
             this.Name = "ProjectTreeMainForm";
             this.Text = "Jira Cloud Tools - Project Tree";
@@ -147,13 +85,8 @@
         #endregion
 
         private Components.JiraConnectBarControl jiraConnectBarControl;
-        private System.Windows.Forms.TreeView treeView;
-        private System.Windows.Forms.ListView listView;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ColumnHeader columnHeader3;
-        private System.Windows.Forms.ColumnHeader columnHeader4;
-        private System.Windows.Forms.ImageList imageList1;
+        private Aga.Controls.Tree.TreeViewAdv treeViewAdv;
+        private Aga.Controls.Tree.NodeControls.NodeTextBox nodeTextBox;
     }
 }
 
--- JiraCloudTools.ProjectTree/ProjectTreeMainForm.cs
+++ JiraCloudTools.ProjectTree/ProjectTreeMainForm.cs
@@ -10,43 +10,67 @@ using System.Windows.Forms;
 using AnotherJiraRestClient;
 using JiraCloudTools.Components;
 using AnotherJiraRestClient.JiraModel;
+using Aga.Controls.Tree;
 
 namespace JiraCloudTools.ProjectTree
 {
     public partial class ProjectTreeMainForm : Form
     {
+        private JiraClient JiraClient { get; set; }
+
         public ProjectTreeMainForm()
         {
             InitializeComponent();
         }
 
+        private int CompareProjects(Project x, Project y)
+        {
+            if (x.projectCategory != null && y.projectCategory != null)
+            {
+                if (x.projectCategory.id.Equals(y.projectCategory.id))
+                {
+                    return string.Compare(x.name, y.name);
+                }
+                else
+                {
+                    return string.Compare(x.projectCategory.name, y.projectCategory.name);
+                }
+            }
+            else if (x.projectCategory == null && y.projectCategory == null)
+            {
+                return string.Compare(x.name, y.name);
+            }
+            else if (x.projectCategory == null && y.projectCategory != null)
+            {
+                return 1;
+            }
+            else if (x.projectCategory != null && y.projectCategory == null)
+            {
+                return -1;
+            }
+            return string.Compare(x.name, y.name);
+        }
+
         private void jiraConnectBarControl_ConnectedToJiraClient(object sender, Components.JiraConnectBarControl.ConnectedEventArgs e)
         {
-            JiraClient jc = e.JiraClient;
+            Cursor.Current = Cursors.WaitCursor;
+            JiraClient = e.JiraClient;
 
-            List<Project> projects = jc.GetProjects();
-            projects.Sort((x, y) => string.Compare(x.name, y.name));
+            List<Project> projects = JiraClient.GetProjects();
+            projects.Sort((x, y) => CompareProjects(x,y));
 
-            treeView.BeginUpdate();
-            treeView.Nodes.Clear();
+            treeViewAdv.BeginUpdate();
+            TreeModel model = new TreeModel();
+            treeViewAdv.Model = model;
+            
             foreach (Project p in projects)
             {
-                treeView.Nodes.Add(string.Format("{0}", p.name));
-                
+                model.Nodes.Add(new Node(p.name));
             }
-            treeView.EndUpdate();
 
-
-
-            listView.BeginUpdate();
-            
-
-            listView.EndUpdate();
+            treeViewAdv.EndUpdate();
         }
 
-        private void listView_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
